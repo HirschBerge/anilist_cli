819c6d515e1571e0de8f88aad8a72d37700878b9
HirschBerge <hskirkwo@geneva.edu>
2024-01-13 15:52:42 -0500

    Progress made.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,9 @@
 // serde_json = "1.0"
 // reqwest = "0.11.8"
 // tokio = { version = "1.0", features = ["macros", "rt-multi-thread"] }
+// main.rs
 
-use reqwest::Client;
-use serde_json::json;
-use std::collections::HashMap;
+mod requests;
 // Query to use in request
 extern crate skim;
 use skim::prelude::*;
@@ -38,77 +37,29 @@ fn fzf(options: Vec<String>) -> Option<String> {
     }
 }
 
-const QUERY: &str = "
-query ($id: Int, $page: Int, $perPage: Int, $search: String) {
-    Page (page: $page, perPage: $perPage) {
-        pageInfo {
-            total
-            currentPage
-            lastPage
-            hasNextPage
-            perPage
-        }
-        media (id: $id, search: $search) {
-            id
-            title {
-                romaji
-                english
-            }
-        }
-    }
-}
-";
-
 #[tokio::main]
 async fn main() {
-    let client = Client::new();
-    let title = "Re:Zero";
-    // Define query and variables
-    let json = json!(
-        {
-            "query": QUERY,
-            "variables": {
-                "search": title,
-                "page": 1,
-                "perPage": 3
-            }
-        }
-    );
-    // Make HTTP post request
-    let resp = client
-        .post("https://graphql.anilist.co/")
-        .header("Content-Type", "application/json")
-        .header("Accept", "application/json")
-        .body(json.to_string())
-        .send()
-        .await
-        .unwrap()
-        .text()
-        .await;
-    let parsed_json: serde_json::Value = serde_json::from_str(&resp.unwrap()).unwrap();
-    // println!("Raw JSON Response: {:#}", resp.unwrap());
-
-    let mut titles_and_ids: HashMap<String, u64> = HashMap::new();
-
-    // Extract relevant information
-    if let Some(media_array) = parsed_json["data"]["Page"]["media"].as_array() {
-        for media_item in media_array {
-            let id = media_item["id"].as_u64().unwrap_or_default();
-
-            // Check if english_title is non-empty
-            if let Some(english_title) = media_item["title"]["english"].as_str() {
-                if !english_title.is_empty() {
-                    // Insert title and ID into the HashMap
-                    titles_and_ids.insert(english_title.to_string(), id);
+    let animes = vec![
+        "Re:Zero: Kara Hajimeru Isekai Seikatsu".to_string(),
+        "Mushoku Tensei: Jobless Reincarnation".to_string(),
+        "Tensei Shitara Slime Datta Ken".to_string(),
+    ];
+    let title = fzf(animes);
+    let mut chosen_id = 0;
+    if let Some(title) = title {
+        match requests::make_graphql_request(&title).await {
+            Ok(titles_and_ids) => {
+                // Print the HashMap
+                for (title, id) in titles_and_ids {
+                    println!("ID: {}, Title: {}", &id, title);
+                    chosen_id = id;
                 }
             }
+            Err(e) => eprintln!("Error making GraphQL request: {:?}", e),
         }
     } else {
-        eprintln!("Invalid JSON format");
+        println!("No title selected");
     }
-
-    // Print the HashMap
-    for (title, id) in titles_and_ids {
-        println!("ID: {}, Title: {}", id, title);
-    }
+    println!("ID is: {}", &chosen_id);
+    requests::print_info(chosen_id).await;
 }
---
+++ src/requests.rs
@@ -0,0 +1,108 @@
+use reqwest::Client;
+use serde_json::json;
+use std::collections::HashMap;
+
+const QUERY_PAGES: &str = "
+query ($id: Int, $page: Int, $perPage: Int, $search: String) {
+    Page (page: $page, perPage: $perPage) {
+        pageInfo {
+            total
+            currentPage
+            lastPage
+            hasNextPage
+            perPage
+        }
+        media (id: $id, search: $search) {
+            id
+            title {
+                romaji
+                english
+            }
+        }
+    }
+}
+";
+const QUERY_INFO: &str = "
+query ($id: Int) { # Define which variables will be used in the query (id)
+  Media (id: $id, type: ANIME) { # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
+    id
+    title {
+      romaji
+      english
+      native
+    }
+    status
+    description
+  }
+}
+";
+
+pub async fn make_graphql_request(title: &str) -> Result<HashMap<String, u64>, reqwest::Error> {
+    let client = Client::new();
+
+    // Define query and variables
+    let json = json!(
+        {
+            "query": QUERY_PAGES,
+            "variables": {
+                "search": title,
+                "page": 1,
+                "perPage": 3
+            }
+        }
+    );
+
+    // Make HTTP post request
+    let resp = client
+        .post("https://graphql.anilist.co/")
+        .header("Content-Type", "application/json")
+        .header("Accept", "application/json")
+        .body(json.to_string())
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    let parsed_json: serde_json::Value = serde_json::from_str(&resp).unwrap();
+
+    let mut titles_and_ids: HashMap<String, u64> = HashMap::new();
+
+    // Extract relevant information
+    if let Some(media_array) = parsed_json["data"]["Page"]["media"].as_array() {
+        for media_item in media_array {
+            let id = media_item["id"].as_u64().unwrap_or_default();
+
+            // Check if english_title is non-empty
+            if let Some(english_title) = media_item["title"]["english"].as_str() {
+                if !english_title.is_empty() {
+                    // Insert title and ID into the HashMap
+                    titles_and_ids.insert(english_title.to_string(), id);
+                }
+            }
+        }
+    } else {
+        eprintln!("Invalid JSON format");
+    }
+
+    Ok(titles_and_ids)
+}
+
+pub async fn print_info(id: u64) {
+    let client = Client::new();
+    // Define query and variables
+    let json = json!({"query": QUERY_INFO, "variables": {"id": id}});
+    // Make HTTP post request
+    let resp = client
+        .post("https://graphql.anilist.co/")
+        .header("Content-Type", "application/json")
+        .header("Accept", "application/json")
+        .body(json.to_string())
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await;
+    // Get json
+    let result: serde_json::Value = serde_json::from_str(&resp.unwrap()).unwrap();
+    println!("{:#}", result);
+}
